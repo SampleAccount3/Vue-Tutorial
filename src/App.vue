959a8b7011198aa389dfec5f38c28d2efb1d3a0e
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-02-13 10:14:51 +0800

    main Sample

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sample sampleMessage="Melanio Flores"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Sample from './components/Sample.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Sample
   }
 }
 </script>
---
+++ src/components/Sample.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>
+            {{sampleMessage}}
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'SampleVue',
+    props:{
+        sampleMessage: String,
+    }
+    
+}
+</script>
